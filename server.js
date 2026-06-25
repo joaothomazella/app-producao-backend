3d11a4dfda88774489a68adb8a1ad0f3126fa5ca
joaothomazella <joaogthomazella2005@gmail.com>
2026-06-25 16:04:18 -0300

    funcionando aplicativo novo

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ const defaultAllowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:5173',
-  'http://127.0.0.1:5500'
+  'http://127.0.0.1:5500',
+  'https://factoryflow-pagina.pages.dev'
 ];
 
 const envAllowedOrigins = (process.env.CORS_ORIGIN || '')
